93c4ed1890f911918b0316a2dc10fd3944b18720
Paul <pavel.k.313@mail.ru>
2022-03-24 17:50:24 +0300

    add db context

---
+++ JweWebApp/Model/JweDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JweWebApp.Model
+{
+    public class JweDbContext : IdentityDbContext
+    {
+        public JweDbContext(DbContextOptions<JweDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
