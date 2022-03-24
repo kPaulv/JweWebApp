7ce53368548449d5ce9116b7162eec2efa19069f
Paul <pavel.k.313@mail.ru>
2022-03-24 18:25:29 +0300

    add register page and register view model

---
+++ JweWebApp/ViewModels/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace JweWebApp.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
