13877a60c61e2e03b1e282d5d9a825a54a026d05
Paul <pavel.k.313@mail.ru>
2022-03-25 17:57:35 +0300

    added refresh token entity

--- JweWebApp/Data/JweDbContext.cs
+++ JweWebApp/Data/JweDbContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using JweWebApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JweWebApp.Data
 {
     public class JweDbContext : IdentityDbContext
     {
+        public virtual DbSet<RefreshTokens> RefreshTokens { get; set; }
         public JweDbContext(DbContextOptions<JweDbContext> options) : base(options)
         {
-
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RefreshTokens>().HasKey(u => u.UserId);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ JweWebApp/Entities/RefreshTokens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JweWebApp.Entities
+{
+    public class RefreshTokens
+    {
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
