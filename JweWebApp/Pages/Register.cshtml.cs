33a4f79b17f1946be249ffe30f4b0f7e4236da2b
Paul <pavel.k.313@mail.ru>
2022-03-25 14:20:35 +0300

    create register page

--- JweWebApp/Pages/Register.cshtml.cs
+++ JweWebApp/Pages/Register.cshtml.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using JweWebApp.ViewModels;
 
 namespace JweWebApp.Pages
 {
     public class RegisterModel : PageModel
     {
+        public Register RegisterViewModel { get; set; }
         public void OnGet()
         {
         }
