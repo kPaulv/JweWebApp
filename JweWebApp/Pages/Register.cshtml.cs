0672fed380718412a8fbed242be9cf8a2e282218
Paul <pavel.k.313@mail.ru>
2022-03-25 14:38:41 +0300

    implement registration

--- JweWebApp/Pages/Register.cshtml.cs
+++ JweWebApp/Pages/Register.cshtml.cs
@@ -1,13 +1,53 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using JweWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace JweWebApp.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        [BindProperty]
         public Register RegisterViewModel { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = RegisterViewModel.Email,
+                    Email = RegisterViewModel.Email
+                };
+
+                var result = await _userManager.CreateAsync(user, RegisterViewModel.Password);
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+
+                foreach(var e in result.Errors)
+                {
+                    ModelState.AddModelError("", e.Description);
+                }
+            }
+
+            return Page();
         }
     }
 }
